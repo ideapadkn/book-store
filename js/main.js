3b33dc2fbf57a815353a59eed14586ec088f5595
Kamron or Камрон <kamrush6429@gmail.com>
2024-07-14 14:13:11 +0500

    response and header hero

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
-$(function () {
-    $('.burger').click(function () {
-        $('.list').toggleClass('show-menu')
-    });
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  autoplay: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
 });
+
+console.log("gg");
